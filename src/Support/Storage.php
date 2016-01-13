aa2225576b1ea2a094275f45256c019e154c1c8c
Ryan Shofner <ryan@letsgolearn.com>
2016-01-12 21:59:54 -0800

    file storage helper class

---
+++ src/Support/Storage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Humweb\SlackPipe\Support;
+
+/**
+ * Storage
+ *
+ * @package Humweb\SlackPipe\Support
+ */
+class Storage
+{
+    protected $path;
+
+    /**
+     * Storage constructor.
+     *
+     * @param string $path
+     */
+    public function __construct($path = null)
+    {
+        $this->path = $path ?: $_SERVER['HOME'].DIRECTORY_SEPARATOR.'.slackpipe'.DIRECTORY_SEPARATOR;
+    }
+
+    public function get($file)
+    {
+        return file_get_contents($this->path($file));
+    }
+
+    public function path($file = '')
+    {
+        return $this->path.$file;
+    }
+
+    public function put($file, $content = '')
+    {
+        return file_put_contents($this->path($file), $content);
+    }
+}

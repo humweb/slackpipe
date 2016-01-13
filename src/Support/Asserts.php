fe2c2cc270d8aaaf6c542579142aee593b7bbd2b
Ryan Shofner <ryan@letsgolearn.com>
2016-01-12 21:56:18 -0800

    assert classes

--- src/Support/Asserts.php
+++ src/Support/Asserts.php
@@ -19,4 +19,29 @@ class Asserts
         return (strpos($url, 'youtube.com') !== false) || (strpos($url, 'youtu.be') !== false) || (strpos($url, 'twitter.') !== false) || (strpos($url,
                 'flickr.') !== false) || (strpos($url, 'xkcd.') !== false);
     }
+
+    /**
+     * @param string $provider
+     *
+     * @return string|bool
+     */
+    static public function hasCustomConfig($provider = '')
+    {
+        $provider = ucfirst($provider);
+        $klass    = 'Humweb\\SlackPipe\\Providers\\'.ucfirst($provider).'\\'.$provider.'Config';
+
+        return class_exists($klass) ? $klass : false;
+    }
+
+    /**
+     * @param string $provider
+     *
+     * @return string|bool
+     */
+    static public function hasCustomSetup($provider)
+    {
+        $klass = 'Humweb\\SlackPipe\\Providers\\'.ucfirst($provider).'\\SetupCommand';
+
+        return class_exists($klass) ? $klass : false;
+    }
 }
